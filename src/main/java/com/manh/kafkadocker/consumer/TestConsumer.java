0f08e0aad32e3c0f4f00d757ec1508cc5879257a
Manh <manhnd4@sapo.vn>
2022-06-16 23:19:36 +0700

    edit dockerfile

--- src/main/java/com/manh/kafkadocker/consumer/TestConsumer.java
+++ src/main/java/com/manh/kafkadocker/consumer/TestConsumer.java
@@ -1,4 +1,14 @@
 package com.manh.kafkadocker.consumer;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TestConsumer {
+
+    @KafkaListener(topics = "server1.dbo.orders", groupId = "Group_id")
+    public void process (String message){
+        System.out.println("Hello kafka order");
+        System.out.println(message);
+    }
 }
